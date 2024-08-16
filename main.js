67b1d15f83c37b3ce4493237dad828b9655585ad
GeniusOfEternity <max06safin@yandex.ru>
2024-08-16 23:01:06 +0300

    version 2.0 - major update of interface and mechanics

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
-const x = 4
-const y = 6
+let time = 61
+let x = 4
+let y = 6
+let currLevel = {}
 const delay = ms => new Promise(res => setTimeout(res, ms))
+const timerHUD = document.getElementById('timer')
 
 const checkMatched = () => {
     const matchedCards = document.querySelectorAll(".matched")
     if (matchedCards.length === x * y) {
-        console.log("GameOver!!")
+        stopTimer()
+        pauseButtons[0].hidden = true
+        let currLevelStats = localStorage.getItem(`${currLevel.name}`)
+        if (currLevelStats) {
+            localStorage.removeItem(`${currLevel.name}`)
+            localStorage.setItem(`${currLevel.name}`, JSON.stringify(parseInt(currLevelStats) + 1))
+        } else {
+            localStorage.setItem(`${currLevel.name}`, JSON.stringify(1))
+        }
+        pauseMenu.classList.add('show')
+        pauseButtons[1].hidden = true
+        timeSpent = 0
+        pauseMenu.children[0].innerHTML = "Solved!!"
     }
 }
 async function lol() {
@@ -19,7 +34,6 @@ async function lol() {
     if (activeCards.length === 2) {
 
         if (activeCards[0].classList[1] === activeCards[1].classList[1]) {
-            console.log("Great!")
             activeCards[0].classList.add('matched')
             activeCards[1].classList.add('matched')
 
@@ -27,8 +41,6 @@ async function lol() {
 
             activeCards[0].removeEventListener("click", lol)
             activeCards[1].removeEventListener("click", lol)
-        } else {
-            console.log("Lose!")
         }
         activeCards[0].classList.remove('active')
         activeCards[1].classList.remove('active')
@@ -38,12 +50,12 @@ async function lol() {
 }
 const makeField = (x, y) => {
     const field = document.querySelector('.field')
-
+    const imgUrl = "media/cards/card-"
     while (field.hasChildNodes()) {
         field.removeChild(field.firstChild)
     }
 
-    const size = 72 / x
+    const size = 80 / x
     const pairs = []
     for (let k = 0; k < x * y / 2; k++) {
         pairs[k] = 2
@@ -68,19 +80,53 @@ const makeField = (x, y) => {
                 }
             }
 
-            back.innerHTML = `${num}`
+            front.innerHTML = `${num}`
+            front.style.backgroundImage = `url("${imgUrl + num}.png")`
 
             card.classList.add(`pair-${num}`)
             card.addEventListener("click", lol)
 
-            card.appendChild(front)
             card.appendChild(back)
+            card.appendChild(front)
             field.appendChild(card)
         }
     }
 }
 
-window.onload = () => {makeField(x, y)}
+let timeSpent = 0
+let t = null
+
+const displayTimer = (timeSpent, time) => {
+    const mins = Math.floor((time - timeSpent) / 60)
+    const secs = (time - timeSpent) % 60
+    mins < 10 ?
+        timerHUD.innerHTML = "0" + mins :
+        timerHUD.innerHTML = "" + mins
+    secs < 10 ?
+        timerHUD.innerHTML += ":0" + secs :
+        timerHUD.innerHTML += ":" + secs
+}
+
+const startTimer = () => {
+    if (timeSpent !== time) {
+        timeSpent++
+        displayTimer(timeSpent, time)
+        t = setTimeout(startTimer, 1000)
+    } else {
+        pauseMenu.classList.add('show')
+        timeSpent = 0
+        pauseButtons[1].hidden = true
+        pauseMenu.children[0].innerHTML = "Failed!!"
+    }
+}
+const stopTimer = () => {
+    clearTimeout(t)
+}
+
+window.onload = () => {
+    musicShuffle(0)
+    setGame()
+}
 
 
 
